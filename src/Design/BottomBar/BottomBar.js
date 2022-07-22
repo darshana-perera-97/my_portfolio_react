fd3bd3924343f0b54c9cebed7e757efe5ef032cb
Darshana Perera <darshana.saluka.10@gmail.com>
2022-07-22 22:34:12 +0530

    bottom bar created

--- src/Design/BottomBar/BottomBar.js
+++ src/Design/BottomBar/BottomBar.js
@@ -2,8 +2,24 @@ import React from "react";
 
 export default function BottomBar() {
   return (
-    <div style={{ position: "absolute", bottom: "0", left: "0" }}>
-      fg
+    <div
+      style={{
+        position: "absolute",
+        bottom: "0",
+        left: "0",
+        background: "#29323c",
+        width: "100%",
+      }}
+    >
+      <p
+        style={{
+          margin: "0px",
+          padding: "10px",
+          color: "rgba(255,255,255,0.5)",
+        }}
+      >
+        <b>Copyright</b> © Darshana Perera
+      </p>
     </div>
   );
 }
