9e0c51dfce105da126b120ffc0d857e5611839fc
Darshana Perera <darshana.saluka.10@gmail.com>
2022-07-22 23:08:17 +0530

    images added

--- src/Design/LeftComponents/Home.js
+++ src/Design/LeftComponents/Home.js
@@ -1,5 +1,12 @@
 import React from "react";
+import background from "./011.png";
+import pro from "./pro.png";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <img src={pro} alt="profile" height={500} />
+      <p>sas</p>
+    </div>
+  );
 }
